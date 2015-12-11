9ffad15f4552a48673619d9d6071be97732b9a29
Guillaume Vincent <guillaume@oslab.fr>
2015-12-11 18:01:07 +0100

    modify app to copy password in clipboard

--- app/app.js
+++ app/app.js
@@ -1,3 +1,13 @@
 import PasswordGenerator from './password-generator';
 
-alert(PasswordGenerator.make_password('password', 'facebook'));
+document.getElementById("help-btn").onclick = function () {
+    $('body').chardinJs('start');
+};
+
+document.getElementById('foo').focus();
+var clipboard = new Clipboard('#copy-btn',{
+    text: function(trigger) {
+        var elements = document.getElementById('foo').value.split(':');
+        return PasswordGenerator.make_password(elements[0], elements[1]);
+    }
+});
