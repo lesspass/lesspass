effbdfd34ff62be1f09297a9015faaaadffe66a1
Guillaume Vincent <guillaume@oslab.fr>
2015-12-11 18:43:55 +0100

    update help on index

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ document.getElementById("help-btn").onclick = function () {
 document.getElementById('foo').focus();
 var clipboard = new Clipboard('#copy-btn',{
     text: function(trigger) {
+        $('body').chardinJs('stop');
         var elements = document.getElementById('foo').value.split(':');
         return PasswordGenerator.make_password(elements[0], elements[1]);
     }
