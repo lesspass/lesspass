096ef344fad1d0e153e2e008a8c8eb42040ac4c3
Guillaume Vincent <guillaume@oslab.fr>
2015-12-17 17:35:52 +0100

    join create_hash and encode inside create password method

--- app/lesspass.js
+++ app/lesspass.js
@@ -1,7 +1,78 @@
 import crypto from 'crypto';
 
-export function make_password(key, text, length = 10) {
-    var password = crypto.createHmac('sha256', key).update(text).digest('hex');
-    return new Buffer(password).toString('base64').substring(0, length);
+export function create_hash(key, text, length = 10, counter = 1) {
+    var password = crypto.createHmac('sha256', key).update(text + counter.toString()).digest('hex');
+    return password.substring(0, length);
 }
 
+export function create_password(master_password, site, password_info) {
+    var hash = create_hash(master_password,site,password_info.size);
+    return encode(hash, password_info.type);
+}
+
+export function string2Uint8Array(text) {
+    var buffer = new ArrayBuffer(text.length);
+    var uint8Array = new Uint8Array(buffer);
+    for (let i = 0; i < text.length; i++) {
+        uint8Array[i] = text.charCodeAt(i);
+    }
+    return uint8Array;
+}
+
+export function getTemplate(passwordType) {
+    var templates = {
+        l: "cv",
+        u: "CV",
+        n: "n",
+        s: "s",
+        lu: "cvCV",
+        ln: "cvn",
+        ls: "cvs",
+        un: "CVn",
+        us: "CVs",
+        ns: "ns",
+        lun: "cvCVn",
+        uns: "CVns",
+        lns: "cvns",
+        lus: "cvCVs",
+        luns: "cvCVns"
+    };
+    return templates[passwordType];
+}
+
+export function encode(hash, passwordType) {
+    var template = getTemplate(passwordType);
+    var password = '';
+    string2Uint8Array(hash).map(
+        (charCode, index) => {
+            var charType = getCharType(template, index);
+            password += getPasswordChar(charType, charCode);
+        }
+    );
+    return password;
+}
+
+function elements_with_index(elements) {
+    return elements.map((element, index) => index + ':' + element);
+}
+
+export var passwordChars = {
+    V: "AEIOUY",
+    C: "BCDFGHJKLMNPQRSTVWXZ",
+    v: "aeiouy",
+    c: "bcdfghjklmnpqrstvwxz",
+    A: "AEIOUYBCDFGHJKLMNPQRSTVWXZ",
+    a: "AEIOUYaeiouyBCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz",
+    n: "0123456789",
+    s: "@&%?,=[]_:-+*$#!'^~;()/.",
+    x: "AEIOUYaeiouyBCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz0123456789@&%?,=[]_:-+*$#!'^~;()/."
+};
+
+export function getPasswordChar(charType, index) {
+    var passwordChar = passwordChars[charType];
+    return passwordChar[index % passwordChar.length];
+}
+
+export function getCharType(template, index) {
+    return template[index % template.length];
+}
--- tests/lesspass.tests.js
+++ tests/lesspass.tests.js
@@ -3,14 +3,100 @@ import * as lesspass from '../app/lesspass';
 
 describe('lesspass', function () {
     it('should return a default password with a size of 10', function () {
-        var generatedPassword = lesspass.make_password('password', 'facebook');
+        var generatedPassword = lesspass.create_hash('password', 'facebook');
         assert.equal(10, generatedPassword.length);
     });
     it('should allow to change password length', function () {
-        var generatedPassword = lesspass.make_password('password', 'facebook', 12);
+        var generatedPassword = lesspass.create_hash('password', 'facebook', 12);
         assert.equal(12, generatedPassword.length);
     });
-    it('should hmac with hash sha256', function () {
-        assert.equal('Y2ViYmVlZm', lesspass.make_password('password', 'facebook'));
+    it('should allow to change password with increment', function () {
+        var password1 = lesspass.create_hash('password', 'facebook', 12);
+        var password2 = lesspass.create_hash('password', 'facebook', 12, 2);
+        assert.notEqual(password1, password2)
+    });
+    it('should create password', function () {
+        var master_password = 'password';
+        var password_info = {
+            'size': '12',
+            'type': 'luns',
+            'counter': '1'
+        };
+        var site = 'facebook';
+        assert.equal('veXU8[syCE4&', lesspass.create_password(master_password, site, password_info));
+    });
+});
+
+
+describe('crypto', function () {
+    it('should convert string into array of char code', function () {
+        var uint8Array = lesspass.string2Uint8Array('ab');
+        assert.equal(97, uint8Array[0]);
+        assert.equal(98, uint8Array[1]);
+        assert.equal('a', String.fromCharCode(uint8Array[0]));
+    });
+
+    describe('templates', function () {
+        it('should get a template from user password type entries', function () {
+            assert.equal('cv', lesspass.getTemplate('l'));
+            assert.equal('CV', lesspass.getTemplate('u'));
+            assert.equal('cvn', lesspass.getTemplate('ln'));
+            assert.equal('ns', lesspass.getTemplate('ns'));
+        });
+        it('should return char inside template based on modulo of the index', function () {
+            var template = 'cv';
+            assert.equal('c', lesspass.getCharType(template, 0));
+            assert.equal('v', lesspass.getCharType(template, 1));
+            assert.equal('c', lesspass.getCharType(template, 10));
+        });
+    });
+
+    describe('encode', function () {
+        it('should return password size same size of hash given', function () {
+            var hash = 'Y2Vi2a112A';
+            var passwordType = 'lun';
+            assert.equal(10, lesspass.encode(hash, passwordType).length);
+        });
+        it('for the same hash should return value depending on the passwordType', function () {
+            var hash = 'a';
+            var passwordType1 = 'n';
+            var passwordType2 = 'l';
+            assert.notEqual(lesspass.encode(hash, passwordType1), lesspass.encode(hash, passwordType2));
+        });
+    });
+
+    describe('getPasswordChar', function () {
+        it('should have vowel type', function () {
+            assert.equal(6, lesspass.passwordChars['v'].length);
+            assert.equal(6, lesspass.passwordChars['V'].length);
+        });
+        it('should have consonant type', function () {
+            assert.equal(20, lesspass.passwordChars['c'].length);
+            assert.equal(20, lesspass.passwordChars['C'].length);
+        });
+        it('should have alphabet uppercase type', function () {
+            assert.equal(26, lesspass.passwordChars['A'].length);
+        });
+        it('should have alphabet uppercase and lowercase type', function () {
+            assert.equal(52, lesspass.passwordChars['a'].length);
+        });
+        it('should have number type', function () {
+            assert.equal(10, lesspass.passwordChars['n'].length);
+        });
+        it('should have symbol type', function () {
+            assert.ok(lesspass.passwordChars['s'].length);
+        });
+        it('should have all type', function () {
+            assert.equal(86, lesspass.passwordChars['x'].length);
+        });
+        it('should return char based on its type and on his index', function () {
+            var typeVowel = 'V';
+            assert.equal('A', lesspass.getPasswordChar(typeVowel, 0));
+        });
+        it('should return char and modulo if overflow', function () {
+            var typeVowel = 'V';
+            assert.equal('E', lesspass.getPasswordChar(typeVowel, 1));
+            assert.equal('E', lesspass.getPasswordChar(typeVowel, 7));
+        });
     });
 });
