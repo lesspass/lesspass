b027cdce0c4e005c74e1792a3a43f0a636aa322a
Guillaume Vincent <guillaume@oslab.fr>
2015-12-19 00:10:40 +0100

    clean application for the first version

--- app/app.js
+++ app/app.js
@@ -1,5 +1,53 @@
-import PasswordGenerator from './password-generator';
+import {lesspass} from '../app/lesspass';
+import angular from 'angular';
 
-console.log(PasswordGenerator.make_password('t','t'));
+class lesspassController {
+    constructor() {
+        this.lowercase = true;
+        this.uppercase = true;
+        this.number = true;
+        this.symbols = true;
+    }
+    displayHelp(){
+        $('body').chardinJs('start');
+    }
+    createPassword() {
+        var passwordTypes = [];
+        if (this.lowercase) {
+            passwordTypes.push('lowercase')
+        }
+        if (this.uppercase) {
+            passwordTypes.push('uppercase')
+        }
+        if (this.number) {
+            passwordTypes.push('number')
+        }
+        if (this.symbols) {
+            passwordTypes.push('symbols')
+        }
+        var site_information = {
+            'site_name': this.site,
+            'password_length': 12,
+            'password_type': passwordTypes,
+            'counter': 1
+        };
 
-import * as stylesheet from './styles/styles.scss';
+        this.generatedPassword = lesspass.create_password(this.password, site_information);
+    }
+}
+
+
+angular
+    .module('app', [])
+    .controller('lesspassController', lesspassController);
+
+
+/*
+var clipboard = new Clipboard('#copy-btn',{
+    text: function(trigger) {
+        $('body').chardinJs('stop');
+        var elements = document.getElementById('foo').value.split(':');
+        return PasswordGenerator.make_password(elements[0], elements[1]);
+    }
+});
+*/
