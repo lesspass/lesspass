0471bac0a302b0a5881cf74dca94f4605188b7e4
Guillaume Vincent <guillaume@oslab.fr>
2015-12-23 16:26:14 +0100

    include vuejs and move to dark template

---
+++ app/components/bootstrap-hr.vue
@@ -0,0 +1,20 @@
+<style>
+    hr {
+        margin-top: 1em;
+        margin-bottom: 3em;
+        border: 0 none -moz-use-text-color;
+        border-top: 1px solid #434857;
+        -moz-border-top-colors: none;
+        -moz-border-right-colors: none;
+        -moz-border-bottom-colors: none;
+        -moz-border-left-colors: none;
+        border-image: none;
+    }
+</style>
+<template>
+    <div class="container">
+        <div class="col-md-6 col-md-offset-3">
+            <hr>
+        </div>
+    </div>
+</template>
