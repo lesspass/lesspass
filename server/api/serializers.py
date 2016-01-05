914bb861922ff1dbaf7f496cfb515ac626bac6f8
Guillaume Vincent <guillaume@oslab.fr>
2016-01-05 21:47:24 +0100

    move server in another git repository

--- server/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from api.models import Site
-
-
-class SiteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Site
-        fields = ('user', 'name', 'password_length', 'password_type', 'counter', 'created', 'modified')
