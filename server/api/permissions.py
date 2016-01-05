44dbc58f8bfa82ac0de4ad0bb0a837d6513aea4a
Guillaume Vincent <guillaume@oslab.fr>
2016-01-05 21:47:24 +0100

    move server in another git repository

--- server/api/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import permissions
-
-
-class IsOwner(permissions.IsAuthenticated):
-    def has_object_permission(self, request, view, obj):
-        return obj.user == request.user
