81e6f6a1c180f9c57a12a617e282464fc5c38cde
Guillaume Vincent <guillaume@oslab.fr>
2016-01-19 09:28:52 +0100

    change logo

--- app/components/header/header.vue
+++ app/components/header/header.vue
@@ -1,23 +1,22 @@
 <style>
-    #header #logo-link {
-        margin: 0 auto;
-        width: 200px;
-        height: 45px;
-    }
-
     #header #logo {
-        width: inherit;
-        height: inherit;
+        width: 30px;
+        height: 30px;
     }
 </style>
 <template>
-    <div id="header">
-        <div class="container text-xs-center">
-            <div id="logo-link">
-                <a href="http://lesspass.com">
-                    <img id="logo" alt="lesspass" class="img-fluid" src="./logo.png">
-                </a>
-            </div>
-        </div>
+    <div id="header" class="container m-t-1">
+        <nav class="navbar navbar-dark ">
+            <a class="navbar-brand" href="#">
+                <img id="logo" alt="lesspass" class="img-fluid" src="./logo.png">
+            </a>
+            <a class="navbar-brand" href="#"> LessPass
+            </a>
+            <ul class="nav navbar-nav pull-xs-right">
+                <li class="nav-item">
+                    <a class="btn btn-primary-outline" href="#/login">Login</a>
+                </li>
+            </ul>
+        </nav>
     </div>
-</template>
+</template>œ
