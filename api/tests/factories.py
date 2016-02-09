962f1ce21ba04b5326c44de32c339a75ad4e5a1b
Guillaume Vincent <guillaume@oslab.fr>
2016-02-09 07:47:39 +0100

    fix error in test factories

--- api/tests/factories.py
+++ api/tests/factories.py
@@ -5,12 +5,8 @@ from api import models
 
 class UserFactory(factory.DjangoModelFactory):
     class Meta:
-        model = models.User
-
-    username = factory.Sequence(lambda n: 'username{0}'.format(n))
-    first_name = factory.Faker('first_name')
-    last_name = factory.Faker('last_name')
-    email = factory.LazyAttribute(lambda a: '{0}.{1}@oslab.fr'.format(a.first_name, a.last_name).lower())
+        model = models.LessPassUser
+    email = factory.Sequence(lambda n: 'u{0}@lesspass.com'.format(n))
     password = factory.PostGenerationMethodCall('set_password', 'password')
     is_staff = False
 
