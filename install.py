a296233acfa5050f18e7885f524ddc97a2272976
Guillaume Vincent <guillaume@oslab.fr>
2016-04-16 13:52:05 +0200

    simplify ssl generation

--- install.py
+++ install.py
@@ -5,55 +5,47 @@ import subprocess
 from dockersible.files import copy, template
 
 
-def copy_certificates():
-    copy(source=(os.path.join('/certificates', os.environ['private_key'])),
-         destination='/etc/ssl/private',
-         basename='private.key',
-         mode='0600')
-    copy(source=os.path.join('/certificates', os.environ['certificate']),
-         destination='/etc/ssl/certs',
-         basename='certificate.crt',
-         mode='0644')
-    context = {
-        'server_name': os.environ['server_name'],
+def get_ssl_context(environ):
+    domain = environ['domain']
+    nginx_info = {
+        'domain': domain,
         'dhparam': False,
         'ssl_trusted_certificate': False
     }
-    cert_folder = os.path.join('/etc/ssl/certs')
-    if 'dhparam' in os.environ:
-        dhparam = os.environ['dhparam']
-        copy(source=os.path.join('/certificates', dhparam), destination=cert_folder, mode='0644')
-        context['dhparam'] = True
-        context['dhparam_path'] = os.path.join(cert_folder, dhparam)
-    if 'certificate' in os.environ:
-        certificate = os.environ['ssl_trusted_certificate']
-        copy(source=os.path.join('/certificates', certificate), destination=cert_folder, mode='0644')
-        context['ssl_trusted_certificate'] = True
-        context['ssl_trusted_certificate_path'] = os.path.join(cert_folder, certificate)
-    return context
+
+    dhparam = os.path.join('/certificates', domain + '.dhparam.pem')
+    if os.path.exists(dhparam):
+        nginx_info['dhparam'] = True
+        copy(source=dhparam, destination='/etc/ssl/certs', basename='dhparam.pem', mode='0644')
+
+    trusted_certificates = os.path.join('/certificates', domain + '.ca.crt')
+    if os.path.exists(trusted_certificates):
+        nginx_info['ssl_trusted_certificate'] = True
+        copy(source=trusted_certificates, destination='/etc/ssl/certs', basename='ca.crt', mode='0644')
+
+    return nginx_info
 
 
-def create_certificates():
-    cmd = """openssl req \
-        -new \
-        -newkey rsa:4096 \
-        -days 365 \
-        -nodes \
-        -x509 \
-        -subj "/C=US/ST=State/L=City/O=Company/CN=%s" \
-        -keyout /etc/ssl/private/private.key \
-        -out /etc/ssl/certs/certificate.crt""".format(os.environ['domain'])
-    subprocess.call(cmd, shell=True)
-    return {
-        'server_name': os.environ['server_name'],
-        'dhparam': False,
-        'ssl_trusted_certificate': False
-    }
+def get_certificates(domain):
+    private_key = os.path.join('/certificates', domain + '.key')
+    certificate = os.path.join('/certificates', domain + '.crt')
+    if not os.path.exists(private_key) or not os.path.exists(certificate):
+        cmd = """openssl req \
+            -new \
+            -newkey rsa:4096 \
+            -days 365 \
+            -nodes \
+            -x509 \
+            -subj "/C=US/ST=State/L=City/O=Company/CN={}" \
+            -keyout {} \
+            -out {}""".format(domain, private_key, certificate)
+        subprocess.call(cmd, shell=True)
+    return private_key, certificate
 
 
 if __name__ == "__main__":
-    if 'private_key' in os.environ and 'certificate' in os.environ:
-        context = copy_certificates()
-    else:
-        context = create_certificates()
-    template('/backend.conf.j2', context, '/etc/nginx/conf.d/backend.conf')
+    pk, crt = get_certificates(os.environ['domain'])
+    copy(source=pk, destination='/etc/ssl/private', basename='private.key', mode='0600')
+    copy(source=crt, destination='/etc/ssl/certs', basename='certificate.crt', mode='0644')
+
+    template('/backend.conf.j2', get_ssl_context(os.environ), '/etc/nginx/conf.d/backend.conf')
