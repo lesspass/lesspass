85f003f7a91c40a7697dfc25826e8f2ac9e19cf6
Guillaume Vincent <guillaume@oslab.fr>
2016-05-14 16:17:12 +0200

    update content script

--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,8 @@
 console.log('content script');
+import {getLoginForm} from './form-parser';
+
+chrome.runtime.onMessage.addListener(message => {
+  const loginForm = getLoginForm();
+  loginForm.passwordField.value = message.password;
+});
+
