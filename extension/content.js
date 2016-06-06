02cd4bd4a423886b38ae40e69801a63f10f2cbc7
Guillaume Vincent <guillaume@oslab.fr>
2016-06-06 17:15:10 +0200

    dispatch input event to simulate user input

--- extension/content.js
+++ extension/content.js
@@ -3,6 +3,11 @@ chrome.runtime.onMessage.addListener(message => {
   const loginForm = getLoginForm();
   loginForm.loginField.value = message.login;
   loginForm.passwordField.value = message.password;
+
+  var event = new Event('input');
+  loginForm.loginField.dispatchEvent(event);
+  loginForm.passwordField.dispatchEvent(event);
+
   loginForm.form.submit();
 });
 
