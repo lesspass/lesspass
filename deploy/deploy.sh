e4bf693e4922af908e16eb036a8dc25f4ccfa809
Guillaume Vincent <guillaume@oslab.fr>
2016-06-18 17:02:51 +0200

    backup db before deploying

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ..
+docker exec -t lesspass_db_1 pg_dumpall -U postgres -c > dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
 export COMPOSE_HTTP_TIMEOUT=120
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml rm -f
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull
