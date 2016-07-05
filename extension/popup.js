70aacd6a93d714733670afc96d53f917a3910c17
Guillaume Vincent <guillaume@oslab.fr>
2016-07-05 12:46:20 +0200

    version 0.0.5

--- extension/popup.js
+++ extension/popup.js
@@ -1,182 +1,50 @@
-import lesspass from 'lesspass';
 import {getDomainName} from './url-parser';
 
-let autoLogin = false;
-const lesspassButton = document.getElementById('lesspassButton');
-
-getCurrentTab().then(currentTab => {
-  chrome.tabs.sendMessage(currentTab.id, {type: 'isThereALoginForm'}, response => {
-    if (response.isThereALoginForm) {
-      lesspassButton.innerText = 'Auto Login';
-      autoLogin = true;
-    } else {
-      lesspassButton.innerText = 'Copy';
-      autoLogin = false;
-    }
-  });
-});
-
-function getStore(key, callback) {
-  chrome.storage.local.get(key, result => {
-    callback(result[key]);
-  });
-}
-
-function saveStore(key, value, callback) {
-  const newStore = {};
-  newStore[key] = value;
-  chrome.storage.local.set(newStore, () => {
-    callback(value);
-  });
-}
-
-function initStore(callback) {
-  getStore('lesspassStore', store => {
-    const defaultStore = Object.assign({
-      login: '',
-      counter: 1,
-      password: {length: 12, settings: ['lowercase', 'uppercase', 'numbers', 'symbols']}
-    }, store);
-
-    saveStore('lesspassStore', defaultStore, store => {
-      callback(store);
-    });
-  });
-}
+var autoLoginButton = document.getElementById('autoLoginButton');
 
 function getCurrentTab() {
-  return new Promise(resolve => {
-    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+  return new Promise(function (resolve) {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
       resolve(tabs[0]);
     });
   });
 }
 
-function fillForm(data) {
-  document.getElementById('loginField').value = data.login;
-  document.getElementById('masterPasswordField').value = '';
-  document.getElementById('siteField').value = data.site;
-  document.getElementById('passwordCounter').value = data.counter;
-
-  const passwordInfo = data.password;
-  document.getElementById('passwordLength').value = passwordInfo.length;
-
-  document.getElementById('lowercase').checked = false;
-  document.getElementById('uppercase').checked = false;
-  document.getElementById('numbers').checked = false;
-  document.getElementById('symbols').checked = false;
-
-  for (let i = 0; i < passwordInfo.settings.length; i++) {
-    document.querySelector(`#${passwordInfo.settings[i]}`).checked = true;
-  }
-}
-
-function selectGoodField() {
-  const loginField = document.getElementById('loginField');
-  const passwordField = document.getElementById('masterPasswordField');
-  if (loginField.value === '') {
-    loginField.focus();
-  } else {
-    passwordField.focus();
-  }
-}
-
-function displayMessage(message, field) {
-  const messageField = document.getElementById(field);
-  messageField.textContent = message;
-}
-
-function getData() {
-  const defaultOptions = {
-    login: document.getElementById('loginField').value,
-    counter: document.getElementById('passwordCounter').value,
-    password: {
-      length: document.getElementById('passwordLength').value,
-      settings: []
+getCurrentTab().then(function (currentTab) {
+  chrome.tabs.sendMessage(currentTab.id, {type: 'isThereALoginForm'}, function (response) {
+    if (typeof response !== 'undefined') {
+      if (response.isThereALoginForm) {
+        autoLoginButton.style.display = 'table-cell';
+      } else {
+        autoLoginButton.style.display = 'none';
+      }
     }
-  };
-  const options = ['lowercase', 'uppercase', 'numbers', 'symbols'];
-
-  for (let i = 0; i < options.length; i++) {
-    if (document.getElementById(options[i]).checked) {
-      defaultOptions.password.settings.push(options[i]);
-    }
-  }
-  return defaultOptions;
-}
-
-function getFormData() {
-  const initData = getData();
-  initData.masterPassword = document.getElementById('masterPasswordField').value;
-  initData.site = document.getElementById('siteField').value;
-  return initData;
-}
-
-document.getElementById('saveDefaultOptionButton').addEventListener('click', () => {
-  const options = getData();
-
-  getStore('lesspassStore', store => {
-    const newStore = Object.assign(store, options);
-
-    saveStore('lesspassStore', newStore, () => {
-      displayMessage('(saved)', 'messageField');
-    });
   });
 });
 
-document.getElementById('generatedPasswordForm').addEventListener('change', generatePassword);
-document.getElementById('passwordLength').addEventListener('input', generatePassword);
-document.getElementById('passwordCounter').addEventListener('input', generatePassword);
-
-function generatePassword() {
-  const data = getFormData();
-  const generatedPasswordField = document.getElementById('generatedPasswordField');
-  if (!data.login || !data.masterPassword || !data.site || !data.password.settings.length) {
-    generatedPasswordField.value = '';
-    return;
-  }
-  return lesspass.generatePassword(data.login, data.masterPassword, data.site, data).then(password => {
-    generatedPasswordField.value = password;
-    return password;
-  });
-}
-
-function copyPassword() {
-  const generatedPasswordField = document.getElementById('generatedPasswordField');
-  generatedPasswordField.select();
-  document.execCommand('copy');
-}
-
-lesspassButton.addEventListener('click', event => {
+autoLoginButton.addEventListener('click', function (event) {
   event.preventDefault();
-  const data = getFormData();
-  if (!data.login || !data.masterPassword || !data.site) {
-    displayMessage('login, master password and site are required to generate a password', 'errorMessageField');
+  var login = document.getElementById('login');
+  var generatedPassword = document.getElementById('generatedPassword');
+  if (!generatedPassword) {
+    console.log('login, master password and site are required to generate a password', 'errorMessageField');
     return;
   }
 
-  const tabPromise = getCurrentTab();
-  const passwordPromise = lesspass.generatePassword(data.login, data.masterPassword, data.site, data);
-  Promise.all([passwordPromise, tabPromise]).then(values => {
-    if (chrome.tabs && chrome.tabs.sendMessage && autoLogin) {
-      chrome.tabs.sendMessage(values[1].id, {
-        type: 'submitForm',
-        login: data.login,
-        password: values[0]
-      });
-      window.close();
-    } else {
-      copyPassword();
-    }
+  getCurrentTab().then(function (currentTab) {
+    chrome.tabs.sendMessage(currentTab.id, {
+      type: 'submitForm',
+      login: login.value,
+      password: generatedPassword.value
+    });
+    window.close();
   });
 });
 
-chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-  if (tabs[0]) {
-    initStore(store => {
-      store.site = getDomainName(tabs[0].url);
-      fillForm(store);
-      selectGoodField();
-    });
+getCurrentTab().then(function (currentTab) {
+  var site = getDomainName(currentTab.url);
+  if (site !== null) {
+    var siteField = document.getElementById('site');
+    siteField.value = site;
   }
 });
