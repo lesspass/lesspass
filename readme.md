8c09fe3f0922625356532e1ab70f220470a2004a
Guillaume Vincent <guillaume@oslab.fr>
2016-07-07 13:07:33 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,25 @@
+# LessPass Pure
+
+LessPass pure html, js and css component used in the web extension and the frontend app.
+
+![LessPass Component](screenshot.png?raw=true "Demo")
+
+# Install
+
+    npm install lesspass-pure
+
+# Test
+
+    npm run dev
+
+open [http://localhost:8080/webpack-dev-server/](http://localhost:8080/webpack-dev-server/)
+
+# Build
+
+    npm run build
+
+# Run
+
+    npm start
+
+see [LessPass](https://github.com/lesspass/lesspass) project
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: {
+    pure: "./src/app",
+    vendors: "./src/vendors"
+  },
+  output: {
+    path: 'dist',
+    publicPath: '/dist/',
+    filename: 'lesspass-[name].js?[hash]'
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
+      {test: /\.scss$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")}
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("lesspass-[name].css")
+  ],
+  devtool: '#eval-source-map'
+};
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map';
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ]);
+}
