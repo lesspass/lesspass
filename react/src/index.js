f867c09c035dcab98f54634318ac13c75798c268
Guillaume Vincent <guillaume@oslab.fr>
2016-09-23 16:44:47 +0200

    remove react folder

--- react/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PasswordGenerator from './components/PasswordGenerator';
-
-ReactDOM.render(
-  <PasswordGenerator />,
-  document.getElementById('root')
-);
