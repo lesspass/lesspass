b20e04ac0a143f592d12aa2275a104b3bd09c3b9
Guillaume Vincent <guillaume@oslab.fr>
2016-10-10 21:05:28 +0200

    load passwords in every page that need it

--- src/components/Passwords.vue
+++ src/components/Passwords.vue
@@ -23,31 +23,55 @@
                 </div>-->
             </div>
         </form>
-        <div id="passwords">
-            <router-link class="list-group-item list-group-item-action"
-                         :to="{ name: 'password', params: { passwordId: password.id }}"
-                         v-for="password in filteredPasswords">
-                <h5 class="list-group-item-heading">{{password.site}}</h5>
-                <p class="list-group-item-text">{{password.login}}</p>
-            </router-link>
+        <div id="passwords" class="row" v-if="!loading">
+            <div class="col-xs-12">
+                <div class="col-xs-12" v-if="passwords.length === 0">
+                    You don't have any passwords saved in your database.
+                    <br>
+                    <router-link :to="{ name: 'home'}">Would you like to create one ?</router-link>
+                </div>
+                <router-link class="list-group-item list-group-item-action"
+                             :to="{ name: 'password', params: { passwordId: password.id }}"
+                             v-for="password in filteredPasswords">
+                    <h5 class="list-group-item-heading">{{password.site}}</h5>
+                    <p class="list-group-item-text">{{password.login}}</p>
+                </router-link>
+            </div>
         </div>
     </div>
 </template>
 <script type="text/ecmascript-6">
-    import {mapGetters} from 'vuex';
+    import Storage from '../api/storage';
+    import HTTP from '../api/http';
+
+    const storage = new Storage();
+    const Passwords = new HTTP('passwords', storage);
 
     export default {
         data(){
             return {
+                passwords: [],
+                loading: true,
                 searchQuery: ''
             }
         },
-        computed: Object.assign(mapGetters(['passwords']), {
+        methods: {
+            fetchPasswords(){
+                Passwords.all().then(response => {
+                    this.passwords = response.data.results;
+                    this.loading = false;
+                });
+            }
+        },
+        computed: {
             filteredPasswords(){
                 return this.passwords.filter(password => {
                     return password.site.indexOf(this.searchQuery) > -1 || password.login.indexOf(this.searchQuery) > -1
                 })
             }
-        })
+        },
+        created: function () {
+            this.fetchPasswords();
+        }
     }
 </script>
