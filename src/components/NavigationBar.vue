72b9ceb81db50249cf57c5df8e36507f3617cfd7
Guillaume Vincent <guillaume@oslab.fr>
2016-10-17 10:03:09 +0200

    update logo

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -14,11 +14,11 @@
             <nav class="nav nav-inline pull-sm-right" style="line-height: 2em">
                 <a class="nav-link white-link"
                    href="https://addons.mozilla.org/en-US/firefox/addon/lesspass/">
-                    <i class="fa fa-firefox" aria-hidden="true"></i>
+                    <i class="fa fa-firefox" aria-hidden="true"></i> Firefox Extension
                 </a>
                 <a class="nav-link white-link"
                    href="https://chrome.google.com/webstore/detail/lesspass/lcmbpoclaodbgkbjafnkbbinogcbnjih">
-                    <i class="fa fa-chrome" aria-hidden="true"></i>
+                    <i class="fa fa-chrome" aria-hidden="true"></i> Chrome Extension
                 </a>
             </nav>
         </div>
