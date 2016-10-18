4a50d90d96d57fd7547532cd55b17c17aa6c393f
Guillaume Vincent <guillaume@oslab.fr>
2016-10-18 19:39:43 +0200

    add DEFAULT_FROM_EMAIL env variable

--- lesspass/settings.py
+++ lesspass/settings.py
@@ -183,6 +183,7 @@ if DEBUG:
     EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
 else:
     EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'contact@lesspass.com')
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'localhost')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
