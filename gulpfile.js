5a3685cb900f630561ed1d3f87d70337f38bffd1
Guillaume Vincent <guillaume@oslab.fr>
2016-10-18 11:54:08 +0200

    use lesspass-pure

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@
 var gulp = require('gulp');
 
 gulp.task('lesspass', [], function () {
-  return gulp.src(['node_modules/lesspass-pure/dist/**/*'])
-    .pipe(gulp.dest('dist/'));
+    return gulp.src(['node_modules/lesspass-pure/dist/**/*'])
+        .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('build', [], function () {
-  gulp.start('lesspass');
+    gulp.start('lesspass');
 });
 
 gulp.task('default', ['build'], function () {
