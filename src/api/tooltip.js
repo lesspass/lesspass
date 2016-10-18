aef30bcf8839e7005fa520b5727676ec70919ff0
Guillaume Vincent <guillaume@oslab.fr>
2016-10-18 11:16:31 +0200

    use lesspass-pure

--- src/api/tooltip.js
+++
@@ -1,8 +0,0 @@
-export function showTooltip(elem, msg) {
-    var classNames = elem.className;
-    elem.setAttribute('class', classNames + ' hint--top');
-    elem.setAttribute('aria-label', msg);
-    setTimeout(function () {
-        elem.setAttribute('class', classNames);
-    }, 2000);
-}
