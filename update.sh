772a4d6c17b84fa37cd315062be6f70cd8754969
Guillaume Vincent <guillaume@oslab.fr>
2016-10-20 16:03:48 +0200

    add update script for config manipulation

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+function cmd {
+    pwd
+}
+
+submodules=( backend cli core cozy frontend nginx pure webextension )
+#submodules=( cozy frontend webextension )
+for submodule in "${submodules[@]}"
+do
+	cd $submodule
+	cmd
+	cd ..
+done
