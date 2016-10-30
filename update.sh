156fb3fbc1046e2cc1fc0a1c7119be6ea14d250d
Guillaume Vincent <guillaume@oslab.fr>
2016-10-30 11:56:43 +0100

    add webcrypto in update.sh script

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ function cmd {
     pwd
 }
 
-submodules=( backend cli core cozy frontend nginx pure webextension )
+submodules=( backend cli core cozy frontend nginx pure webcrypto webextension )
 #submodules=( cozy frontend webextension )
 for submodule in "${submodules[@]}"
 do
