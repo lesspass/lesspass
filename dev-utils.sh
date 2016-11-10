c82a0acbbdd6e6f51fbf37c7fac79fea42c90d08
Guillaume Vincent <guillaume@oslab.fr>
2016-11-10 17:01:22 +0100

    update sh utils scripts

---
+++ dev-utils.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+function cmd {
+    pwd
+}
+
+submodules=( backend cli core cozy frontend nginx pure webcrypto webextension )
+#submodules=( cozy frontend webextension )
+for submodule in "${submodules[@]}"
+do
+	cd $submodule
+	cmd
+	cd ..
+done
+
+
+#VERSION=1.1.2
+#cd webextension
+#git archive -o webextension.zip master
+#mv webextension.zip /tmp
+#cd ../pure/
+#git archive -o pure.zip master
+#mv pure.zip /tmp
+#cd /tmp
+#mkdir LessPass
+#mv pure.zip LessPass/
+#mv webextension.zip LessPass/
+#cd LessPass/
+#unzip pure.zip -d pure
+#unzip webextension.zip -d webextension
+#rm pure.zip webextension.zip
+#zip -r LessPass-src-v$VERSION.zip pure webextension
+#mv LessPass-src-v$VERSION.zip ~/Desktop/
+#cd ~/Desktop/
+#rm -rf /tmp/LessPass
