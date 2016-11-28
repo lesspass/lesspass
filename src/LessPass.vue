58a30eb7d1de88d0ca34c6f8cb848027e0dc480f
Guillaume Vincent <guillaume@oslab.fr>
2016-11-28 17:57:08 +0100

    remove ugly border color

--- src/LessPass.vue
+++ src/LessPass.vue
@@ -5,16 +5,7 @@
 
     #lesspass.card {
         border: none;
-    }
-
-    @media (min-width: 544px) {
-        #lesspass.v1 {
-            border: 1px solid #f0ad4e;
-        }
-
-        #lesspass.v2 {
-            border: 1px solid #0275d8;
-        }
+        box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
     }
 
     #lesspass .white-link:hover, #lesspass .white-link:focus, #lesspass .white-link:active {
