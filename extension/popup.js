c475eb34423723774080d69b3e6093b8a51b9025
Guillaume Vincent <guillaume@oslab.fr>
2016-12-12 11:50:22 +0100

    add loading icon to make app loading faster

--- extension/popup.js
+++ extension/popup.js
@@ -1,8 +1,7 @@
-setTimeout(function () {
-    var site = document.getElementById('site');
-    if (site.value.length > 0) {
-        document.getElementById('login').focus();
-    } else {
-        site.focus();
-    }
-}, 500);
+document.addEventListener("DOMContentLoaded", function () {
+    let head = document.getElementsByTagName('head')[0];
+    let script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'dist/lesspass.min.js';
+    head.appendChild(script);
+});
