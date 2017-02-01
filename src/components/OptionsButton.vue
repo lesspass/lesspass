8a1e916c23bca7114b7b66c01cac1ce3e95cb36b
Guillaume Vincent <guillaume@oslab.fr>
2017-02-01 23:08:41 +0100

    make option button normal again

--- src/components/OptionsButton.vue
+++ src/components/OptionsButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button type="button" class="btn btn-secondary px-2 px-sm-3">
+    <button type="button" class="btn btn-secondary">
         <i class="fa fa-sliders" aria-hidden="true"></i>
     </button>
 </template>
