21cc3a9d4fae8fd72424da924390686586e6fa89
Guillaume Vincent <guillaume@oslab.fr>
2017-02-02 16:24:00 +0100

    simplify password getter

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,18 +1,12 @@
 export const passwords = state => state.passwords;
 
+export const password = state => state.password;
+
 export const isAuthenticated = state => state.authenticated;
 
 export const isGuest = state => !state.authenticated;
 
 export const passwordStatus = state => state.passwordStatus;
-
-export const password = state => {
-    const tenMinutesAgo = new Date().getTime() - 10 * 60 * 1000;
-    if (state.lastUse < tenMinutesAgo) {
-        return Object.assign({}, state.defaultPassword);
-    }
-    return Object.assign({}, state.defaultPassword, state.password);
-};
 
 export const version = state => {
     if(state.password === null){
