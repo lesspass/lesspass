ae96c6f5224cf8b288fca1936f4fb2e65084ce48
Guillaume Vincent <guillaume@oslab.fr>
2017-02-03 23:31:27 +0100

    auto save default options and remove save button

--- src/views/ConfigureOptions.vue
+++ src/views/ConfigureOptions.vue
@@ -1,17 +1,21 @@
 <template>
     <div>
-        <h4>Default Options</h4>
-        <small class="help-text">default options are saved locally</small>
+        <div class="form-group">
+            <input id="login"
+                   name="login"
+                   type="text"
+                   class="form-control"
+                   placeholder="Login"
+                   autocomplete="off"
+                   autocorrect="off"
+                   autocapitalize="none"
+                   v-model="defaultPassword.login">
+        </div>
         <options v-bind:password="defaultPassword" v-on:optionsUpdated="updatePassword"></options>
         <div class="form-group">
-            <button type="button" class="btn btn-primary" v-on:click="saveDefault">
-                <span v-if="optionsSaved">
-                   <i class="fa fa-check" aria-hidden="true"></i> saved
-                </span>
-                <span v-else>
-                  Save
-                </span>
-            </button>
+            <div class="alert alert-info" role="alert">
+                Default options are saved <strong>locally</strong></small>
+            </div>
         </div>
     </div>
 </template>
@@ -28,22 +32,12 @@
         computed: mapGetters(['defaultPassword']),
         data(){
             return {
-                options: null,
                 optionsSaved: false
             }
         },
         methods: {
-            saveDefault(){
-                if (this.options !== null) {
-                    this.$store.dispatch('saveDefaultPassword', {password: this.options});
-                }
-                this.optionsSaved = true;
-                setTimeout(() => {
-                    this.optionsSaved = false;
-                }, 3000);
-            },
             updatePassword(password){
-                this.options = password;
+                this.$store.dispatch('saveDefaultPassword', {password});
             }
         }
     }
