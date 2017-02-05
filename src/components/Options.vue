46b9c4598461ae3c9affaa50e31ea3019c8304e7
Guillaume Vincent <guillaume@oslab.fr>
2017-02-05 11:22:36 +0100

    add field hint

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -54,7 +54,8 @@
                 </div>
             </div>
             <div class="col-6 col-sm-4 mb-3 mb-sm-0">
-                <label for="passwordCounter">Counter</label>
+                <label for="passwordCounter"
+                class="hint--top hint--medium" aria-label="Increment counter field to change generated password without changing your master password.">Counter</label>
                 <div class="input-group input-group-sm">
                     <span class="input-group-btn" v-on:click="options.counter-=1">
                         <button class="btn btn-secondary" type="button"><i class="fa fa-minus"></i></button>
@@ -83,7 +84,8 @@
 
                     </div>
                     <div class="col-6">
-                        <button type="button" class="btn btn-block btn-sm border-left-0"
+                        <button type="button" class="btn btn-block btn-sm border-left-0 hint--top-left hint--medium hint--error"
+                                aria-label="Version 1 is deprecated and will be removed. We strongly advise you to use version 2."
                                 v-bind:class="{'btn-warning':options.version===1,'btn-secondary':options.version!==1}"
                                 v-on:click="setVersion(1)">
                             v<span class="hidden-sm-up">ersion </span>1
