c128c96dfa8fe2d8411f3cdb5da003301b790ddd
Guillaume Vincent <guillaume@oslab.fr>
2017-03-13 14:37:03 +0100

    add .editorconfig and auto format

--- main.js
+++ main.js
@@ -7,25 +7,25 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({width: 600, height: 475, icon: path.resolve(__dirname, 'images/icon.png')});
+  mainWindow = new BrowserWindow({width: 600, height: 475, icon: path.resolve(__dirname, 'images/icon.png')});
 
-    mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-    mainWindow.on('closed', function () {
-        mainWindow = null;
-    });
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
 }
 
 app.on('ready', createWindow);
 
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
-app.on('activate', function () {
-    if (mainWindow === null) {
-        createWindow();
-    }
+app.on('activate', function() {
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
