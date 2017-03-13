3a74ac32907b4b6d9ff6ccc2be71e0babcbf8770
Guillaume Vincent <guillaume@oslab.fr>
2017-03-13 14:36:34 +0100

    add .editorconfig and auto format

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var app = express();
 
 app.use('/dist', express.static(__dirname + '/dist'));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(8080, function () {
-    console.log('frontend listening on port 8080');
+app.listen(8080, function() {
+  console.log('frontend listening on port 8080');
 });
