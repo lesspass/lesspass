e30cc4b1bcd169d0a6818b4118aea2cef735f483
Guillaume Vincent <guillaume@oslab.fr>
2017-03-13 14:37:09 +0100

    add .editorconfig and auto format

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,15 @@
 
 const gulp = require('gulp');
 
-gulp.task('lesspass', [], function () {
-    return gulp.src(['node_modules/lesspass-pure/dist/**/*'])
-        .pipe(gulp.dest('www/dist/'));
+gulp.task('lesspass', [], function() {
+  return gulp.src(['node_modules/lesspass-pure/dist/**/*'])
+    .pipe(gulp.dest('www/dist/'));
 });
 
-gulp.task('build', [], function () {
-    gulp.start('lesspass');
+gulp.task('build', [], function() {
+  gulp.start('lesspass');
 });
 
-gulp.task('default', ['build'], function () {
+gulp.task('default', ['build'], function() {
 
 });
