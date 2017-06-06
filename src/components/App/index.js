9302abdf54a3f03f8e7acb6524dbbfdfb7a94062
Guillaume Vincent <guillaume@oslab.fr>
2017-06-06 18:37:57 +0200

    bootstrap react app

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>LessPass Move</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
