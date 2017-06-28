38148a2fe54392dfb086eb27d6d227ffaf718b57
Guillaume Vincent <guillaume@oslab.fr>
2017-06-29 00:35:59 +0200

    ::S:

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../docs/index.html'),
     assetsRoot: path.resolve(__dirname, '../docs'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: '',
     assetsPublicPath: '/move/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
