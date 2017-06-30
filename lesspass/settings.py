f49e6e4d2d1267914bae7fe004c321ff06629b73
Guillaume Vincent <guillaume@oslab.fr>
2017-06-30 10:29:17 +0200

    Add basic authentication for LessPass Move

--- lesspass/settings.py
+++ lesspass/settings.py
@@ -137,6 +137,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_RENDERER_CLASSES': (
