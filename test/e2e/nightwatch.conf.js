79cbef6fb046041347f5e8650244e5e13474c4fb
Guillaume Vincent <guillaume@oslab.fr>
2017-07-26 08:29:19 +0200

    Cable end to end tests with travis

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -16,9 +16,9 @@ module.exports = {
       },
       desiredCapabilities: {
         browserName: "chrome",
-        // chromeOptions: {
-        //   args: ['--headless','--no-sandbox','--disable-gpu','--window-size=1920x1080']
-        // }
+        chromeOptions: {
+          args: ['--headless','--no-sandbox','--disable-gpu','--window-size=1920x1080']
+        }
       }
     }
   }
