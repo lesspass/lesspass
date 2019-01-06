4211b7c80612493114844d08658307dad3c5e002
Guillaume Vincent <guillaume@oslab.fr>
2019-01-06 09:33:31 +0100

    Add cli in continuous integration

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 set -e
 
 python3 --version
+
+# containers
 pushd containers
 export COMPOSE_PROJECT_NAME=lesspass
 docker-compose build
@@ -9,4 +11,14 @@ docker-compose up -d
 docker exec -it lesspass_backend_1 sh -c 'python3 wait_db.py'
 docker exec -it lesspass_backend_1 sh -c 'python3 manage.py test'
 docker-compose down
+popd
+
+# cli
+pushd cli
+rm -rf venv
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt -r test-requirements.txt
+tox
+deactivate
 popd
