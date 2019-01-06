35941e5585f9de4b3b51987d425a8778d4491e6d
Guillaume Vincent <guillaume@oslab.fr>
2019-01-06 10:33:57 +0100

    Add docker tests

--- containers/test.sh
+++ containers/test.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-python3 --version
-
 # containers
-pushd containers
 export COMPOSE_PROJECT_NAME=lesspass
 docker-compose build
 docker-compose up -d
 docker exec -it lesspass_backend_1 sh -c 'python3 wait_db.py'
 docker exec -it lesspass_backend_1 sh -c 'python3 manage.py test'
 docker-compose down
-popd
