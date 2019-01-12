feb5aa899f0d631f1a7437e21ce64e0c45939e35
Salim B <salim@posteo.de>
2019-01-12 16:51:47 +0000

    Improve grammar and style of the Help screen in lesspass-mobile

--- packages/lesspass-mobile/src/help/HelpScreen.js
+++ packages/lesspass-mobile/src/help/HelpScreen.js
@@ -16,20 +16,21 @@ export default class HelpScreen extends Component {
         <Title style={{ marginBottom: 10 }}>LessPass</Title>
         <Subheading>How does it works?</Subheading>
         <Paragraph style={{ marginBottom: 20 }}>
-          LessPass is a password manager that doesn't save any data. The site,
-          the login and the master password are mashed together with encryption
-          to create a secure password. It will always generate the same password
-          as long as those 3 parameters don't change.
+          LessPass is a password manager that doesn't store any data. It computes
+          a unique password using a site, login and a master password. You don't
+          need to sync a password vault across every device because LessPass works
+          offline! It will always generate the same password as long as those three
+          parameters don't change.
         </Paragraph>
         <Subheading>Master password emoticons</Subheading>
         <Image source={require("./fingerprint.png")} />
         <Paragraph style={{ marginBottom: 20 }}>
           Your master password is the only thing you should keep in your head.
-          The emoticons on the right let you verify if you typed in the right
-          master password. You will have to wait a second or so before it shows
-          the good emoticons. Otherwise it'd be possible for a shoulder-peeker
-          to guess the password character-by-character instead of all at once,
-          with the former being trivial and the later impossible.
+          The emoticons on the right let you verify that you typed in the right
+          master password. You will have to wait a second or so before the final
+          emoticons appear (the delay is for security; if the emoticons were
+          shown instantly, a shoulder-peeker could derive your password based
+          on the series of displayed emoticons).
         </Paragraph>
         <Subheading>Options</Subheading>
         <Image
@@ -41,34 +42,37 @@ export default class HelpScreen extends Component {
         />
         <Paragraph style={{ marginBottom: 20 }}>
           Sometimes sites have specific password rules. For instance, some banks
-          only accept passwords made of digits. The application let you set
-          parameters for the generated password. Use the counter if you want to
-          change the generated password without changing your master password.
+          only accept passwords made of digits. LessPass lets you set parameters for
+          the generated password (the so called "password profile"). The counter in
+          particular allows you to generate a new password without having to change
+          your master password.
         </Paragraph>
         <Subheading>Sign In</Subheading>
         <Paragraph style={{ marginBottom: 10 }}>
-          LessPass by default doesn't save any data. But you can if you want use
-          LessPass in a connected mode. This mode helps you to save password
-          profile needed to regenerate some passwords. A password’s profile is
-          everything except the master password and the generated password.
-          There is no critical information (a generated password encrypted for
-          example) in the LessPass database.
+          By default, LessPass works offline and doesn't send any data over the
+          network. For convenience, there's an optional connected mode. This mode
+          allows you to store the profiles necessary to generate your passwords on
+          our (or alternatively your own) LessPass server. This is especially useful
+          if many of your passwords are not based on the default profile (length of
+          16 characters, all characters allowed). As a password profile does neither
+          include the master password nor the generated password, there is no
+          critical information sent to the server.
         </Paragraph>
         <Paragraph style={{ marginBottom: 20 }}>
-          The sign in form ask for your master password. We use your master
-          password to create a LessPass generated password using a default
-          password profile. The master password is never send on our servers. If
-          you don't want to encrypt your passord, you can disable this option in
-          the settings
+          The sign-in form asks for your master password. By default, it is used to
+          generate a password for your LessPass account in the same way as for any other
+          site. This means the master password itself is never sent to our servers.
+          If for any reason you don't want to use such a LessPass-generated password
+          for your LessPass account, you can disable this in the settings.
         </Paragraph>
-        <Subheading>Self Hosted LessPass Database</Subheading>
+        <Subheading>Self-hosted LessPass Database</Subheading>
         <Paragraph style={{ marginBottom: 20 }}>
-          If you are using a self hosted LessPass database, you can update the
-          base url in the settings.
+          If you are using a self-hosted LessPass database, you can change the base
+          url in the settings to point to your own server.
         </Paragraph>
         <Subheading>Sign Out</Subheading>
         <Paragraph style={{ marginBottom: 20 }}>
-          You can sign out using the Sign Out button in the settings
+          You can sign out using the Sign Out button in the settings.
         </Paragraph>
         <Subheading>Contributing</Subheading>
         <Paragraph>You can read our contributing guide:</Paragraph>
@@ -88,7 +92,7 @@ export default class HelpScreen extends Component {
         </Button>
         <Subheading>Support</Subheading>
         <Paragraph>
-          You still need some help? No problem, you can send us an email at
+          Still need some help? No problem, you can send us an email at
           contact@lesspass.com. You can write your email in english or french.
         </Paragraph>
         <Button
