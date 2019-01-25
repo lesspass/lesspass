65cdbda0222bd910efb250ca056d6234037c6e8e
Guillaume Vincent <guillaume@oslab.fr>
2019-01-25 11:35:38 +0100

    Add auto push on docker registry

--- containers/backend/entrypoint.sh
+++ containers/backend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
+python3 wait_db.py
 python3 manage.py migrate
 python3 manage.py collectstatic --clear --no-input
-python3 wait_db.py 
 
 exec "$@"
--- containers/test.sh
+++ containers/test.sh
@@ -5,6 +5,8 @@ set -e
 export COMPOSE_PROJECT_NAME=lesspass
 docker-compose build
 docker-compose up -d
+date -u
 docker exec -it lesspass_backend_1 sh -c 'python3 wait_db.py'
+date -u
 docker exec -it lesspass_backend_1 sh -c 'python3 manage.py test'
 docker-compose down
