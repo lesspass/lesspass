a3037df29591ff86dab85c5fc78b3d3b847288a7
Édouard Lopez <edouard-lopez@users.noreply.github.com>
2019-01-28 08:48:31 +0100

    Moar test on password generation (#380)
    * test _calc_entropy()
    
    * test _get_configured_rules()
    
    * test _get_set_of_characters()
    
    * test _consume_entropy()
    
    * test _get_one_char_per_rule()
    
    * test _insert_string_pseudo_randomly()
    
    * test _render_password()

--- cli/tests/test_password_generation.py
+++ cli/tests/test_password_generation.py
@@ -1,6 +1,7 @@
 import unittest
 
 from lesspass.password import generate_password
+from lesspass import password
 
 
 class TestPassword(unittest.TestCase):
@@ -81,3 +82,93 @@ class TestPassword(unittest.TestCase):
         self.assertEqual(
             generate_password(profile, master_password), "XFt0F*,r619:+}[."
         )
+
+    def test_calc_entropy(self):
+        password_profile = {
+            "site": "example.org",
+            "login": "contact@example.org",
+            "counter": 1,
+        }
+        master_password = 'password'
+
+        self.assertEqual(
+            password._calc_entropy(password_profile, master_password), b'dc33d431bce2b01182c613382483ccdb0e2f66482cbba5e9d07dab34acc7eb1e'
+        )
+
+    def test_get_configured_rules_empty_when_no_rules_in_profile(self):
+        password_profile = {}
+
+        self.assertListEqual(password._get_configured_rules(password_profile), [])
+
+    def test_get_configured_rules_ignore_disable_rules(self):
+        password_profile = {
+            "lowercase": False,
+            "uppercase": True,
+            "digits": False,
+            "symbols": True,
+        }
+
+        self.assertListEqual(password._get_configured_rules(password_profile), ['uppercase', 'symbols'])
+
+    def test_get_set_of_characters_without_rule(self):
+        self.assertEqual(password._get_set_of_characters(), 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~')
+        
+    def test_get_set_of_characters_with_single_rule(self):
+        self.assertEqual(password._get_set_of_characters(["lowercase"]), "abcdefghijklmnopqrstuvwxyz")
+        self.assertEqual(password._get_set_of_characters(["uppercase"]), "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        self.assertEqual(password._get_set_of_characters(["digits"]), "0123456789")
+        self.assertEqual(password._get_set_of_characters(["symbols"]), "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+        
+    def test_get_set_of_characters_with_several_rules(self):
+        self.assertEqual(password._get_set_of_characters(["lowercase", "digits"]), "abcdefghijklmnopqrstuvwxyz0123456789")
+
+    def test_consume_entropy(self):
+        entropy = b'dc33d431bce2b01182c613382483ccdb0e2f66482cbba5e9d07dab34acc7eb1e'
+
+        password_value, password_entropy = password._consume_entropy( 
+            generated_password="", 
+            quotient=int(entropy, 16), 
+            set_of_characters="abcdefghijklmnopqrstuvwxyz0123456789", 
+            max_length=12
+        )
+
+        self.assertEqual(password_value, 'gsrwvjl3d0sn')
+        self.assertEqual(password_entropy, 21019920789038193790619410818194537836313158091882651458040)
+
+    def test_get_one_char_per_rule_without_rules(self):
+        self.assertListEqual(
+            password._get_one_char_per_rule( entropy=21019920789038193790619410818194537836313158091882651458040, rules=[] ),
+            ['', 21019920789038193790619410818194537836313158091882651458040]
+        )
+
+    def test_get_one_char_per_rule_with_several_rules(self):
+        self.assertListEqual(
+            password._get_one_char_per_rule(
+                entropy=21019920789038193790619410818194537836313158091882651458040, 
+                rules=['lowercase', 'digits']
+            ),
+            ['a0', 80845849188608437656228503146902068601204454199548659454] 
+        )
+
+    def test_insert_string_pseudo_randomly(self):
+        self.assertEqual(
+            password._insert_string_pseudo_randomly(
+                generated_password='gsrwvjl3d0sn', 
+                entropy=80845849188608437656228503146902068601204454199548659454, 
+                string='a0'
+            ), 'gsrwvjl03d0asn'
+        )
+
+    def test_render_password(self):
+        password_profile = { 
+            "site": "example.org",
+            "login": "contact@example.org",
+            "digits": True,
+            "lowercase": True,
+            "length": 14,
+            "counter": 1
+        }
+        master_password = 'password'
+        entropy = password._calc_entropy(password_profile, master_password)
+
+        self.assertEqual(password._render_password(entropy, password_profile), 'gsrwvjl03d0asn')
