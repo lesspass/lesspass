b310f00cb8610f7a3ceb252b4fae885dbe6382cb
Guillaume Vincent <guillaume@oslab.fr>
2019-02-20 11:12:46 +0100

    Format update

--- cli/tests/test_interaction.py
+++ cli/tests/test_interaction.py
@@ -3,9 +3,10 @@ import pexpect
 import signal
 import time
 
+
 class TestInteraction(unittest.TestCase):
     def test_keyboard_interrupt(self):
-        p = pexpect.spawn('python3 lesspass/core.py --prompt')
+        p = pexpect.spawn("python3 lesspass/core.py --prompt")
         p.expect("Site: ")
         p.kill(signal.SIGINT)
         p.expect(pexpect.EOF)
