166fa24ff7499b389f8add4e624b76fa3e2afc32
Guillaume Vincent <guillaume@oslab.fr>
2019-06-11 18:33:47 +0200

    Stop loading async translation

--- packages/lesspass-pure/src/main.js
+++ packages/lesspass-pure/src/main.js
@@ -6,21 +6,31 @@ import store from "./store";
 import router from "./router";
 import Polyglot from "vue-polyglot";
 
+import frLocales from "./i18n/fr.json";
+import esLocales from "./i18n/es.json";
+import deLocales from "./i18n/de.json";
+import zhLocales from "./i18n/zh.json";
+import zhCNLocales from "./i18n/zh-CN.json";
+import ptLocales from "./i18n/pt.json";
+
 Vue.use(Polyglot, {
   defaultLanguage: "en",
   languagesAvailable: ["fr", "es", "de", "zh", "zh-CN", "pt"]
+});
+
+Vue.locales({
+  fr: frLocales,
+  es: esLocales,
+  de: deLocales,
+  zh: zhLocales,
+  "zh-CN": zhCNLocales,
+  pt: ptLocales
 });
 
 sync(store, router);
 
 new Vue({
   el: "#lesspass",
-  beforeCreate() {
-    this.$polyglot.getLocale({
-      baseURL: "dist/i18n",
-      lang: "auto"
-    });
-  },
   store,
   router,
   render: h => h(LessPass)
