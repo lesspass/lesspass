b053cd34228169037db477467d9c195873a4b106
Daniel Koć <daniel@xn--ko-wla.pl>
2019-06-16 03:46:26 +0200

    Adding Polish translation

--- packages/lesspass-pure/scripts/buildI18n.js
+++ packages/lesspass-pure/scripts/buildI18n.js
@@ -21,7 +21,7 @@ walker.on("file", (root, fileStats, next) => {
 });
 
 const I18N_DIR = "../src/i18n";
-const LANGUAGES_AVAILABLE = ["zh", "zh-CN", "fr", "es", "de", "en", "pt"];
+const LANGUAGES_AVAILABLE = ["zh", "zh-CN", "fr", "es", "de", "en", "pt", "pl"];
 walker.on("end", () => {
   LANGUAGES_AVAILABLE.forEach(lang => {
     const localeFile = path.join(I18N_DIR, `${lang}.json`);
