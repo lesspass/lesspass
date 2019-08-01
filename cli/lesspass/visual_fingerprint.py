2d1bf6932cd9efeb670860ee07c6320590d79f19
Chandler DeLoach <deloachcd@g.cofc.edu>
2019-08-01 09:58:33 -0600

    Added icons-in-terminal support.

--- cli/lesspass/visual_fingerprint.py
+++ cli/lesspass/visual_fingerprint.py
@@ -11,11 +11,26 @@ if os.name == "nt":
     import msvcrt
 
 
-def user_has_unifont():
-    # TODO: actual implementation of this
-    return False
+def user_has_icons_in_terminal():
+    return os.path.exists(os.path.expanduser("~/.fonts/icons-in-terminal.ttf"))
 
 
+colors_256 = [
+    "\x1b[38;5;248m",  # black               #000000
+    "\x1b[38;5;30m",  # dark cyan           #074750
+    "\x1b[38;5;37m",  # mid cyan            #009191
+    "\x1b[38;5;211m",  # bright pink         #FF6CB6
+    "\x1b[38;5;219m",  # cotton candy pink   #FFB5DA
+    "\x1b[38;5;55m",  # mid purple          #490092
+    "\x1b[38;5;69m",  # sky blue            #006CDB
+    "\x1b[38;5;140m",  # lavendar            #B66DFF
+    "\x1b[38;5;81m",  # baby blue           #6DB5FE
+    "\x1b[38;5;153m",  # white blue          #B5DAFE
+    "\x1b[38;5;88m",  # blood red           #920000
+    "\x1b[38;5;94m",  # burnt orange        #924900
+    "\x1b[38;5;172m",  # orange              #DB6D00
+    "\x1b[38;5;82m",  # lime green          #24FE23
+]
 unicode_colors = [
     "\x1b[31;40m",  # black               #000000
     "\x1b[36;40m",  # dark cyan           #074750
@@ -48,7 +63,7 @@ fallback_colors = [
     "\x1b[30;43m",  # orange              #DB6D00
     "\x1b[30;42m",  # lime green          #24FE23
 ]
-icons = [
+icon_names = [
     "hashtag",
     "heart",
     "hotel",
@@ -96,54 +111,56 @@ icons = [
     "gamepad",
     "graduation-cap",
 ]
-icons_unicode = {
-    "hashtag": "\u0023",
-    "heart": "\u2665",
-    "hotel": "\u1F3E8",
-    "university": "\u1F3DB",
-    "plug": "\u1F50C",
-    "ambulance": "\u1F691",
-    "bus": "\u1F68C",
-    "car": "\u1F697",
-    "plane": "\u2708",
-    "rocket": "\u1F680",
-    "ship": "\u1F6A2",
-    "subway": "\u1F687",
-    "truck": "\u26DF",
-    "japanese yen": "\u00A5",
-    "euro": "\u20AC",
-    "bitcoin": "\u20BF",
-    "U.S. dollar": "\u0024",
-    "British pound": "\u00A3",
-    "archive": "\u1F5C3",
-    "area-chart": "\u1F4C8",
-    "bed": "\u1F6CF",
-    "beer": "\u1F37A",
-    "bell": "\u1F514",
-    "binoculars": "\u1F52D",  # NOTE: "Telescope" substituted
-    "birthday-cake": "\u1F382",
-    "bomb": "\u1F4A3",
-    "briefcase": "\u1F4BC",
-    "bug": "\u1F41B",
-    "camera": "\u1F4F7",
-    "cart-plus": "\u1F6D2",  # NOTE: "Shopping Trolley" substituted
-    "certificate": "\u1F7D3",  # NOTE: "Heavy Tweleve Pointed Black Star" substituted
-    "coffee": "\u2615",
-    "cloud": "\u2601",
-    "comment": "\u1F5E9",
-    "cube": "\u1F4E6",  # NOTE: "Package" subtituted
-    "cutlery": "\u1F374",
-    "database": "\u26C1",
-    "diamond": "\u25C6",
-    "exclamation-circle": "\u2757",  # NOTE: "Heavy Exclamation Mark" substituted
-    "eye": "\u1F441",
-    "flag": "\u2691",
-    "flask": "\u1F376",  # NOTE: "Sake Bottle and Cup" subtituted
-    "futbol": "\u26BD",
-    "gamepad": "\u1F3AE",
-    "graduation-cap": "\u1F393",
+icons_in_terminal_icons = {
+    "hashtag": "\ue33e",
+    "heart": "\ue0e5",
+    "hotel": "\ue268",  # NOTE: "fa-building" substituted
+    "university": "\ue644",
+    "plug": "\ue29d",
+    "ambulance": "\ue1bf",
+    "bus": "\ue2bc",
+    "car": "\ue587",
+    "plane": "\ue14c",
+    "rocket": "\ue1f7",
+    "ship": "\ue2ce",
+    "subway": "\ue2eb",
+    "truck": "\ue199",
+    "japanese yen": "\uec97",  # NOTE: Linea circled yen icon substituted
+    "euro": "\ue714",
+    "bitcoin": "\ue21a",
+    "U.S. dollar": "\ue215",
+    "British pound": "\uec89",  # NOTE: Linea circled pound sterling icon substituted
+    "archive": "\ue244",
+    "area-chart": "\ue2b4",
+    "bed": "\ue2e8",
+    "beer": "\ue1c2",
+    "bell": "\ue1b9",
+    "binoculars": "\ue29c",
+    "birthday-cake": "\ue2b3",
+    "bomb": "\ue299",
+    "briefcase": "\ue187",
+    "bug": "\ue245",
+    "camera": "\ue10e",
+    "cart-plus": "\ue2cb",
+    "certificate": "\ue17a",
+    "coffee": "\ue1ba",
+    "cloud": "\ue18b",
+    "comment": "\ue14f",
+    "cube": "\ue26c",
+    "cutlery": "\ue1bb",
+    "database": "\ue279",
+    "diamond": "\ue2cd",
+    "exclamation-circle": "\ue145",
+    "eye": "\ue149",
+    "flag": "\ue102",
+    "flask": "\ue18c",
+    "futbol": "\ue29a",
+    "gamepad": "\ue1df",
+    "graduation-cap": "\ue259",
 }
-MAX_ICON_WIDTH = max([len(icon) for icon in icons])
+
+
+MAX_ICON_WIDTH = max([len(icon) for icon in icon_names])
 
 
 def get_list_entry(hash_slice, lookup_list):
@@ -152,14 +169,23 @@ def get_list_entry(hash_slice, lookup_list):
 
 
 def get_color(hash_slice):
-    return get_list_entry(
-        hash_slice, unicode_colors if user_has_unifont() else fallback_colors
-    )
+    if user_has_icons_in_terminal():
+        if "256" in os.environ["TERM"]:
+            colors = colors_256
+        else:
+            colors = unicode_colors
+    else:
+        colors = fallback_colors
+    return get_list_entry(hash_slice, colors)
 
 
 def get_icon(hash_slice):
-    icon_name = get_list_entry(hash_slice, icons)
-    return icons_unicode[icon_name] if user_has_unifont() else icon_name
+    icon_name = get_list_entry(hash_slice, icon_names)
+    return (
+        icons_in_terminal_icons[icon_name]
+        if user_has_icons_in_terminal()
+        else icon_name
+    )
 
 
 def get_fingerprint(hmac_sha256):
@@ -177,8 +203,8 @@ def get_hmac_sha256(password_bytes):
 
 def get_fixed_width_text(fingerprint_entry):
     color, icon = fingerprint_entry["color"], fingerprint_entry["icon"]
-    if user_has_unifont():
-        text = f"[{color}{icon}{' '*(2-len(icon))}\x1b[0m]"
+    if user_has_icons_in_terminal():
+        text = f"{color}{icon} \x1b[0m"
     else:
         text = f"{color}{icon}{' '*(MAX_ICON_WIDTH-len(icon))}\x1b[0m"
     return text
@@ -187,9 +213,9 @@ def get_fixed_width_text(fingerprint_entry):
 def get_mnemonic(password):
     fingerprint = get_fingerprint(get_hmac_sha256(password.encode("utf-8")))
     return (
-        f"{get_fixed_width_text(fingerprint[0])} "
+        f"[ {get_fixed_width_text(fingerprint[0])} "
         f"{get_fixed_width_text(fingerprint[1])} "
-        f"{get_fixed_width_text(fingerprint[2])}"
+        f"{get_fixed_width_text(fingerprint[2])} ]"
     )
 
 
@@ -234,7 +260,7 @@ def getpass_with_visual_fingerprint(prompt):
         if delayed_write:
             delayed_write.cancel()
         if c == "\r":
-            sys.stdout.write(f"\r{prompt}{get_fake_mnemonic()}{' '*3}\n")
+            sys.stdout.write(f"\r{prompt}{get_fake_mnemonic()}\n")
             break
         elif c == "\x7f":  # backspace
             password = password[:-1]
@@ -242,13 +268,12 @@ def getpass_with_visual_fingerprint(prompt):
             password += c
         if len(password) != 0:
             delayed_write = threading.Timer(
-                0.5,
-                lambda: sys.stdout.write(f"\r{prompt}{get_mnemonic(password)}{' '*3}"),
+                0.5, lambda: sys.stdout.write(f"\r{prompt}{get_mnemonic(password)}")
             )
             delayed_write.start()
-            sys.stdout.write(f"\r{prompt}{get_fake_mnemonic()}{' '*3}")
+            sys.stdout.write(f"\r{prompt}{get_fake_mnemonic()}")
         else:
-            sys.stdout.write(f"\r{prompt}{' '*((MAX_ICON_WIDTH*3)+3)}")
+            sys.stdout.write(f"\r{prompt}{' '*(MAX_ICON_WIDTH*3)}")
     return password
 
 
