1fff70620c79aec888fb9cb92ceedee777ddff69
William Hiver <william.hiver@free.fr>
2019-10-15 21:41:53 +0200

    First version of the extension options page (#479)
    * WIP adding a preference page
    
    * Add the login field
    
    * First working version
    
    * Propagate the default login to the popup
    
    * Update build scripts
    
    * Remove the option page entry point
    
    * Remove unecessary storage permission
    
    * Code cleanup
    
    * Remove dependency to defaultPassword.js
    
    * Reset dist folder
    
    * Move the page to lesspass-pure
    
    * Add translations

---
+++ packages/lesspass-pure/src/views/OptionsPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <form id="lesspass-options-form" novalidate v-on:submit.prevent="saveOptions">
+        <div class="form-group row full-width">
+          <label for="login" class="col-sm-2 col-form-label">{{$t('Default login')}}</label>
+          <div class="col-sm-10">
+            <input
+              id="login"
+              type="text"
+              name="login"
+              ref="login"
+              class="form-control"
+              autocomplete="off"
+              autocorrect="off"
+              autocapitalize="none"
+              v-model="defaultPassword.login"
+            />
+          </div>
+        </div>
+        <div class="form-group row">
+          <div class="col-sm-10">
+            <button type="submit" class="btn btn-primary">{{$t('Save')}}</button>
+          </div>
+        </div>
+      </form>
+</template>
+
+<script>
+  import { mapState } from "vuex";
+  import { SET_DEFAULT_OPTIONS } from "../store/mutation-types";
+
+  export default {
+    computed: mapState(["defaultPassword"]),
+    methods: {
+      saveOptions() {
+        this.$store.commit(SET_DEFAULT_OPTIONS, this.defaultPassword);
+      }
+    }
+  }
+</script>
