1b7a60961280acbf50e70d0d521262bf548626ad
Guillaume Vincent <guillaume@oslab.fr>
2019-10-20 09:18:24 +0200

    Migrate lesspass-pure to vue-cli and apply prettier

--- packages/lesspass-pure/src/components/InputSite.vue
+++ packages/lesspass-pure/src/components/InputSite.vue
@@ -1,10 +1,10 @@
 <style>
-  .awesomplete mark {
-    background-color: transparent !important;
-    padding: 0;
-    margin: 0;
-    color: inherit;
-  }
+.awesomplete mark {
+  background-color: transparent !important;
+  padding: 0;
+  margin: 0;
+  color: inherit;
+}
 </style>
 <template>
   <div class="inputSite">
@@ -20,69 +20,74 @@
         autocorrect="off"
         autocapitalize="none"
         v-bind:placeholder="label"
-        v-model="site">
+        v-model="site"
+      />
     </div>
   </div>
 </template>
 <script>
-  import Awesomplete from "awesomplete";
-  import {getSuggestions} from "../services/url-parser";
+import Awesomplete from "awesomplete";
+import { getSuggestions } from "../services/url-parser";
 
-  export default {
-    name: "inputSite",
-    props: {
-      value: String,
-      label: String,
-      passwords: {
-        type: Array,
-        default: () => []
+export default {
+  name: "inputSite",
+  props: {
+    value: String,
+    label: String,
+    passwords: {
+      type: Array,
+      default: () => []
+    }
+  },
+  mounted() {
+    this.awesomplete = new Awesomplete(this.$refs.siteField);
+    this.awesomplete.item = (element, input) => {
+      let item = Awesomplete.ITEM(element.value.site, input);
+      item.innerHTML += ` ${element.value.login}`;
+      return item;
+    };
+    this.awesomplete.filter = (site, input) => {
+      return (
+        Awesomplete.FILTER_CONTAINS(site, input) ||
+        Awesomplete.FILTER_CONTAINS(input, site)
+      );
+    };
+    this.awesomplete.data = data => {
+      return { label: data.site, value: data };
+    };
+    this.awesomplete.replace = password => {
+      this.$refs.siteField.value = password.label;
+      if (password.value.suggestion) {
+        this.$emit("suggestionSelected", password.value.site);
+      } else {
+        this.$emit("passwordProfileSelected", password.value);
       }
-    },
-    mounted() {
-      this.awesomplete = new Awesomplete(this.$refs.siteField);
-      this.awesomplete.item = (element, input) => {
-        let item = Awesomplete.ITEM(element.value.site, input);
-        item.innerHTML += ` ${element.value.login}`;
-        return item;
-      };
-      this.awesomplete.filter = (site, input) => {
-        return Awesomplete.FILTER_CONTAINS(site, input) ||
-          Awesomplete.FILTER_CONTAINS(input, site);
-      };
-      this.awesomplete.data = data => {
-        return {label: data.site, value: data}
-      };
-      this.awesomplete.replace = password => {
-        this.$refs.siteField.value = password.label;
-        if (password.value.suggestion) {
-          this.$emit("suggestionSelected", password.value.site);
-        } else {
-          this.$emit("passwordProfileSelected", password.value);
-        }
-      };
-      this.awesomplete.sort = (a,b) => {
-        return a.value.site.localeCompare(b.value.site) ||
-          a.value.login.localeCompare(b.value.login);
+    };
+    this.awesomplete.sort = (a, b) => {
+      return (
+        a.value.site.localeCompare(b.value.site) ||
+        a.value.login.localeCompare(b.value.login)
+      );
+    };
+  },
+  computed: {
+    site: {
+      get: function() {
+        return this.value;
+      },
+      set: function(newValue) {
+        this.$emit("input", newValue);
       }
-    },
-    computed: {
-      site: {
-        get: function () {
-          return this.value;
-        },
-        set: function (newValue) {
-          this.$emit("input", newValue);
-        }
-      }
-    },
-    watch: {
-      site: function (newValue, _) {
-        const suggestions = getSuggestions(newValue).map(suggestion => {
-          return {site: suggestion, suggestion: true, login: ''}
-        });
-        this.awesomplete.list = this.passwords.concat(suggestions);
-      }
-    },
-    methods: {}
-  };
+    }
+  },
+  watch: {
+    site: function (newValue) {
+      const suggestions = getSuggestions(newValue).map(suggestion => {
+        return { site: suggestion, suggestion: true, login: "" };
+      });
+      this.awesomplete.list = this.passwords.concat(suggestions);
+    }
+  },
+  methods: {}
+};
 </script>
--- packages/lesspass-pure/src/components/PasswordProfile.vue
+++ packages/lesspass-pure/src/components/PasswordProfile.vue
@@ -1,23 +1,23 @@
 <style>
-  .passwordProfile {
-    display: flex;
-    cursor: pointer;
-    margin-bottom: 1rem;
-    justify-content: space-between;
-    align-items: center;
-  }
+.passwordProfile {
+  display: flex;
+  cursor: pointer;
+  margin-bottom: 1rem;
+  justify-content: space-between;
+  align-items: center;
+}
 
-  .passwordProfile__info {
-    display: flex;
-    align-items: center;
-    flex-grow: 1;
-  }
+.passwordProfile__info {
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+}
 
-  .passwordProfile__meta {
-    font-size: 0.8rem;
-    line-height: 1rem;
-    flex-grow: 1;
-  }
+.passwordProfile__meta {
+  font-size: 0.8rem;
+  line-height: 1rem;
+  flex-grow: 1;
+}
 </style>
 <template>
   <div class="passwordProfile">
@@ -25,39 +25,41 @@
       <avatar v-bind:name="password.site"></avatar>
       <div class="passwordProfile__meta">
         <b>{{password.site}}</b>
-        <br>
+        <br />
         {{password.login}}
       </div>
     </div>
     <div class="passwordProfile__actions">
-      <i class="passwordProfile__delete-icon fa fa-trash fa-fw text-danger"
-         v-on:click="deletePassword()"></i>
+      <i
+        class="passwordProfile__delete-icon fa fa-trash fa-fw text-danger"
+        v-on:click="deletePassword()"
+      ></i>
     </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
-  import Avatar from './Avatar.vue';
+import Avatar from "./Avatar.vue";
 
-  export default {
-    name: 'passwordProfile',
-    props: {
-      password: {
-        type: Object,
-        required: true
-      }
+export default {
+  name: "passwordProfile",
+  props: {
+    password: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    Avatar
+  },
+  methods: {
+    deletePassword() {
+      this.$store.dispatch("deletePassword", { id: this.password.id });
     },
-    components: {
-      Avatar
-    },
-    methods: {
-      deletePassword() {
-        this.$store.dispatch('deletePassword', {id: this.password.id});
-      },
-      setPassword() {
-        this.$store.dispatch('savePassword', {password: this.password});
-        this.$router.push({name: 'home'});
-      }
+    setPassword() {
+      this.$store.dispatch("savePassword", { password: this.password });
+      this.$router.push({ name: "home" });
     }
   }
+};
 </script>
 
