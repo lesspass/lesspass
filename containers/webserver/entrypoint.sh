e063e38355117ecd13c1a3f6e8af2843fa96f32e
Guillaume Vincent <guillaume@oslab.fr>
2020-04-18 21:59:01 +0200

    wip refactor containers

--- containers/webserver/entrypoint.sh
+++ containers/webserver/entrypoint.sh
@@ -1,27 +1,8 @@
 #!/usr/bin/env bash
 
-create_wildcard_certificate () {
-  openssl req -x509 -newkey rsa:4096 -nodes -keyout ${1}.key -out ${1}.crt -days 365 -subj "/C=FR/ST=Gironde/L=Bordeaux/O=LessPass/OU=LessPass/CN=*.${1}"
-}
+sed -i "s~FQDN~${FQDN}~" conf/httpd.conf conf/extra/httpd-ssl.conf
+sed -i "s~EMAIL~${EMAIL}~" conf/httpd.conf conf/extra/httpd-ssl.conf
+sed -i "s~CRT_PATH~${CRT_PATH}~" conf/extra/httpd-ssl.conf
+sed -i "s~KEY_PATH~${KEY_PATH}~" conf/extra/httpd-ssl.conf
 
-if [[ ! -f /app/ssl/${FQDN}.crt || ! -f /app/ssl/${FQDN}.key ]]; then
-  echo "${FQDN}.crt or ${FQDN}.key not found! Generate wildcard certificate"
-  cd /app/ssl
-  create_wildcard_certificate ${FQDN}
-fi
-
-mkdir -p /etc/httpd/ssl
-chmod 755 /etc/httpd/ssl
-cp /app/ssl/${FQDN}.crt /etc/httpd/ssl/
-chmod 644 /etc/httpd/ssl/${FQDN}.crt
-
-mkdir -p /etc/httpd/ssl/private
-chmod 710 /etc/httpd/ssl/private
-cp /app/ssl/${FQDN}.key /etc/httpd/ssl/private/
-chmod 640 /etc/httpd/ssl/private/${FQDN}.key
-
-python /app/generate_apache_conf.py
-
-cat /etc/httpd/conf.d/lesspass.conf
-
-exec /usr/sbin/httpd -D FOREGROUND
+exec "$@"
