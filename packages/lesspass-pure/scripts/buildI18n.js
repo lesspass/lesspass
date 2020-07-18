1d7cf0dd510391945575c7757b812cbe302f65c0
Minobi <30632135+Minobi@users.noreply.github.com>
2020-07-18 20:18:25 +0300

    Add Russian translation
    Add Russian translation into lesspass-pure package

--- packages/lesspass-pure/scripts/buildI18n.js
+++ packages/lesspass-pure/scripts/buildI18n.js
@@ -21,7 +21,7 @@ walker.on("file", (root, fileStats, next) => {
 });
 
 const I18N_DIR = "../src/i18n";
-const LANGUAGES_AVAILABLE = ["zh", "zh-CN", "fr", "es", "de", "en", "pt", "pl"];
+const LANGUAGES_AVAILABLE = ["zh", "zh-CN", "fr", "es", "de", "en", "pt", "pl", "ru"];
 walker.on("end", () => {
   LANGUAGES_AVAILABLE.forEach(lang => {
     const localeFile = path.join(I18N_DIR, `${lang}.json`);
