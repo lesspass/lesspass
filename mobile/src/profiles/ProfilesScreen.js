3557e68e3d77dd0b07f7505ac76b3b187fb80a91
Guillaume Vincent <guillaume@oslab.fr>
2021-10-28 23:36:11 +0200

    Add search in password profiles page

--- mobile/src/profiles/ProfilesScreen.js
+++ mobile/src/profiles/ProfilesScreen.js
@@ -10,6 +10,7 @@ import {
   Provider,
   Paragraph,
   Button,
+  Searchbar,
 } from "react-native-paper";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -18,15 +19,21 @@ import {
 } from "../password/profilesActions";
 import routes from "../routes";
 import Styles from "../ui/Styles";
+import fuzzysort from "fuzzysort";
 import { cleanPasswordProfile, setPasswordProfile } from "./profileActions";
 import { sortByNewestFirst } from "./sort";
 
 export default function ProfilesScreen() {
   const navigation = useNavigation();
   const dispatch = useDispatch();
-  const profiles = useSelector((state) => state.profiles);
+  const profiles = useSelector((state) => Object.values(state.profiles));
   const [profileToDelete, setProfileToDelete] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
+  const results =
+    query === ""
+      ? profiles
+      : fuzzysort.go(query, profiles, { key: "site" }).map((r) => r.obj);
 
   const _getPasswordProfilesCallback = useCallback(() => {
     setIsLoading(true);
@@ -73,37 +80,59 @@ export default function ProfilesScreen() {
             </Dialog.Actions>
           </Dialog>
         </Portal>
-        <List.Section title="Password profiles">
-          {isLoading ? null : Object.keys(profiles).length === 0 ? (
+        <List.Section>
+          {isLoading ? null : profiles.length === 0 ? (
             <List.Item
               description={
                 "You don't have any password profiles. Save a password profile when you generate it."
               }
             />
           ) : (
-            sortByNewestFirst(Object.values(profiles)).map((profile) => (
-              <View key={profile.id}>
-                <List.Item
-                  title={profile.site}
-                  description={profile.login}
-                  onPress={() => {
-                    dispatch(setPasswordProfile(profile));
-                    navigation.navigate(routes.PASSWORD_GENERATOR);
-                  }}
-                  right={(props) => (
-                    <IconButton
-                      {...props}
-                      icon="delete"
-                      onPress={(event) => {
-                        setProfileToDelete(profile);
-                        event.stopPropagation();
-                      }}
-                    />
-                  )}
+            <View>
+              <View style={{ paddingHorizontal: 10 }}>
+                <Searchbar
+                  placeholder={`Search in your ${profiles.length} profile${
+                    profiles.length === 1 ? "" : "s"
+                  }`}
+                  onChangeText={setQuery}
+                  value={query}
+                  autoCapitalize="none"
+                  autoCorrect={false}
                 />
-                <Divider />
               </View>
-            ))
+              {results.length === 0 ? (
+                <List.Item
+                  description={
+                    "There is no password profile that matches this search."
+                  }
+                />
+              ) : (
+                sortByNewestFirst(results).map((profile) => (
+                  <View key={profile.id}>
+                    <List.Item
+                      title={profile.site}
+                      description={profile.login}
+                      onPress={() => {
+                        dispatch(setPasswordProfile(profile));
+                        setQuery("");
+                        navigation.navigate(routes.PASSWORD_GENERATOR);
+                      }}
+                      right={(props) => (
+                        <IconButton
+                          {...props}
+                          icon="delete"
+                          onPress={(event) => {
+                            setProfileToDelete(profile);
+                            event.stopPropagation();
+                          }}
+                        />
+                      )}
+                    />
+                    <Divider />
+                  </View>
+                ))
+              )}
+            </View>
           )}
         </List.Section>
       </ScrollView>
