bc56ee052a910c3598f55a73a66ab8ea82dbecf9
Guillaume Vincent <guillaume@oslab.fr>
2022-02-17 13:57:32 +0100

    Remove sandbox projects

--- sandbox/lesspass-web-component/src/passwords/PasswordsPage.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const PasswordsPage = () => <div>PasswordsPage</div>;
-
-export default PasswordsPage;
--- sandbox/lesspass-web-component/src/settings/defaultSettings.ts
+++
@@ -1,7 +0,0 @@
-const defaultSettings: Settings = {
-  saveMasterPassword: true,
-  useMasterPasswordForAuth: true,
-  language: null,
-};
-
-export default defaultSettings;
