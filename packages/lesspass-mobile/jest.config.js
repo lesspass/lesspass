c9cd07d065ae72b0199e382e8b9a0706e31dbeab
Guillaume Vincent <guillaume@oslab.fr>
2024-07-23 14:43:36 +0200

    Rebuild modile app to remove internal testing on Android store

--- packages/lesspass-mobile/jest.config.js
+++ packages/lesspass-mobile/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: 'react-native',
-  
+  setupFiles: ['<rootDir>/src/setup-jest.js'],
+  transform: {
+    '\\.js$': '<rootDir>/node_modules/babel-jest',
+  },
 };
---
+++ packages/lesspass-mobile/src/setup-jest.js
@@ -0,0 +1,1 @@
+import 'react-native-gesture-handler/jestSetup';
