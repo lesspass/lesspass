03911cef3ab609359946845f5e0c672e61e6da60
Guillaume Vincent <guillaume@oslab.fr>
2025-03-01 09:26:48 +0100

    Fix reset settings using the entire store instead of settings

--- packages/lesspass-web-component/src/services/constant.ts
+++ packages/lesspass-web-component/src/services/constant.ts
@@ -1,4 +1,4 @@
-export const LESSPASS_SETTINGS = "LessPassSettings";
+export const LESSPASS_SETTINGS = "LessPassSettings2";
 export const ACCESS_TOKEN_LOCAL_STORAGE_KEY = "LessPassAccessToken";
 export const REFRESH_TOKEN_LOCAL_STORAGE_KEY = "LessPassRefreshToken";
 export const BASE_URL_LOCAL_STORAGE_KEY = "LessPassBaseURL";
--- packages/lesspass-web-component/src/settings/settingsMiddleware.ts
+++ packages/lesspass-web-component/src/settings/settingsMiddleware.ts
@@ -1,6 +1,7 @@
 import { createListenerMiddleware } from "@reduxjs/toolkit";
-import { setSettings, resetSettings, SettingsState } from "./settingsSlice";
+import { setSettings, resetSettings } from "./settingsSlice";
 import { saveSettings } from "../services/settings";
+import { RootState } from "../store";
 
 const settingsMiddleware = createListenerMiddleware();
 
@@ -15,8 +16,8 @@ settingsMiddleware.startListening({
 settingsMiddleware.startListening({
   actionCreator: resetSettings,
   effect: (_, listenerApi) => {
-    const state = listenerApi.getState() as SettingsState;
-    saveSettings(state);
+    const state = listenerApi.getState() as RootState;
+    saveSettings(state.settings);
   },
 });
 
