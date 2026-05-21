8dec6fe7d761bf9337cb7c84897a417c081b08ed
markoulinoss <t8230101@aueb.gr>
2026-05-21 20:04:26 +0100

    Add password strength indicator with i18n support for mobile and web version

--- packages/lesspass-mobile/src/password/MasterPassword.js
+++ packages/lesspass-mobile/src/password/MasterPassword.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { View, NativeModules } from "react-native";
+import zxcvbn from "zxcvbn";
+import { withTranslation } from "react-i18next";
+import { Text } from "react-native-paper";
 import TextInput from "../ui/TextInput";
 import TouchId from "./TouchId";
 import Fingerprint from "./Fingerprint";
 import { createFingerprint } from "lesspass/fingerprint";
 import { debounce } from "lodash";
 
-export default class MasterPassword extends Component {
+class MasterPassword extends Component {
   constructor(props) {
     super(props);
     this.state = { fingerprint: null };
@@ -47,8 +50,20 @@ export default class MasterPassword extends Component {
       hideFingerprint,
       label = "Master Password",
       style = {},
+      t,
     } = this.props;
     const { fingerprint } = this.state;
+    const score = masterPassword && masterPassword.length > 0 ? zxcvbn(masterPassword).score : -1;
+
+    const strengthLabels = [
+      t("PasswordStrength.Score0"),
+      t("PasswordStrength.Score1"),
+      t("PasswordStrength.Score2"),
+      t("PasswordStrength.Score3"),
+      t("PasswordStrength.Score4"),
+    ];
+    const strengthColors = ["#6b7280", "#ef4444", "#f97316", "#3b82f6", "#22c55e"];
+
     return (
       <View>
         <TextInput
@@ -60,6 +75,11 @@ export default class MasterPassword extends Component {
           onSubmitEditing={this.props.onSubmitEditing}
           outerRef={this.props.outerRef}
         />
+        {score >= 0 && (
+          <Text style={{ color: strengthColors[score], fontSize: 12, marginTop: 4 }}>
+            {strengthLabels[score]}
+          </Text>
+        )}
         {masterPassword && fingerprint ? (
           <Fingerprint
             style={{ position: "absolute", right: 10, top: 24, bottom: 0 }}
@@ -72,3 +92,5 @@ export default class MasterPassword extends Component {
     );
   }
 }
+
+export default withTranslation()(MasterPassword);
